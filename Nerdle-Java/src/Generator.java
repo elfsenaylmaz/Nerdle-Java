647520c0c24708a9ad4a51a80630b1354886d18c
Engin <engin.3769@gmail.com>
2022-05-28 23:49:43 +0300

    Fixed 7DigitEquation

--- Nerdle-Java/src/Generator.java
+++ Nerdle-Java/src/Generator.java
@@ -1,82 +1,136 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Generator {
 	private char[] operators = {'+', '-', '*', '/'};
 	private Random random;
-	private String equation = "";
+	private String equation;
+	ArrayList<Integer> operandsList;
+	ArrayList<Character> operatorsList;
+	private Solver solver;
 	
 	public Generator() {
 		random = new Random();
+		operandsList = new ArrayList<Integer>();
+		operatorsList = new ArrayList<Character>();
+		solver = new Solver();
 	}
 	
-	public String generate7DigitEquation() {
-		int operand;
+	public void create7DigitString() {
+		int operand, newOperand;
         char operator;
         int type;
         int numberOfOperator;
+        
+        operandsList.clear();
+        operatorsList.clear();
+        equation = "";
+        
         type = random.nextInt(2);									// = sembolunun yerlesecegi kisimi secmek
         if(type == 0){
             numberOfOperator = random.nextInt(2);					// denklemde kac tane operator olacaginin secimi
             if(numberOfOperator == 1){								//2 operator olmasi durumu
                 operand = random.nextInt(10);
+                operandsList.add(operand);
                 equation += operand;
 
                 operator = operators[random.nextInt(4)];
+                operatorsList.add(operator);
                 equation += operator;
                 if(operator == '/')
                     operand = random.nextInt(1,10);
                 else
                     operand = random.nextInt(10);
+                operandsList.add(operand);
                 equation += operand;
 
                 operator = operators[random.nextInt(4)];
+                operatorsList.add(operator);
                 equation += operator;
                 if(operator == '/')
                     operand = random.nextInt(1,10);
                 else
                     operand = random.nextInt(10);
+                operandsList.add(operand);
                 equation += operand;
             }
             else{																//tek operator olmasi durumu
                 operand = random.nextInt(10,100);
+                operandsList.add(operand);
                 equation += operand;
                 do {
                     operator = operators[random.nextInt(4)];
                 }while (operator == '+' || operator == '*');
+                operatorsList.add(operator);
                 equation += operator;
                 if(operator == '-')
-                    equation += random.nextInt(operand - 9,operand + 1);
-                else
-                    equation += random.nextInt(10,100);
+                    operand = random.nextInt(operand - 9, operand + 1);
+                else {
+                	do {
+                		newOperand = random.nextInt(10, operand + 1);
+                	}while(operand % newOperand != 0);           
+                	operand = newOperand;
+                }                
+                operandsList.add(operand);
+                equation += operand;
             }
         }
         else{
             if(random.nextInt(2) == 0){
                 operand = random.nextInt(10,100);
+                operandsList.add(operand);
                 equation += operand;
                 operator = operators[random.nextInt(4)];
+                operatorsList.add(operator);
                 equation += operator;
-                if(operator == '/')
-                    operand = random.nextInt(1,10);
+                if(operator == '/') {
+                	do {
+                		newOperand = random.nextInt(1,10);
+                	}while(operand % newOperand != 0);
+                	operand = newOperand;
+                }
                 else
                     operand = random.nextInt(10);
+                operandsList.add(operand);
                 equation += operand;
             }
             else{
-                equation += random.nextInt(10,100);
+                operand = random.nextInt(10,100);
+                operandsList.add(operand);
+                equation += operand;
                 do{
                     operator = operators[random.nextInt(4)];
                 }while(operator == '-' || operator == '/');
+                operatorsList.add(operator);
                 equation += operator;
                 if(operator == '/')
                     operand = random.nextInt(1,10);
                 else
                     operand = random.nextInt(10);
+                operandsList.add(operand);
                 equation += operand;
             }
         }
-
-        return equation;
+        //return equation;
 	}
-
+	
+	public String generate7DigitEquation() {
+		int result;
+		String newEquation;
+		do {
+			do {
+				create7DigitString();
+				solver.setList(operandsList, operatorsList);
+			}while(!solver.calculate());
+			result = solver.getResult();
+			newEquation = equation;
+			newEquation += '=';
+			newEquation += result;
+		}while(newEquation.length() != 7);
+		equation = newEquation;
+		return equation;
+	}
+	
+	
+	
 }
---
+++ Nerdle-Java/src/Solver.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solver {
+	private Stack<Integer> operandStack;
+	private Stack<Character> operatorStack;
+	private ArrayList<Integer> operands;
+	private ArrayList<Character> operators;
+	private int number1, number2;
+    private int result;
+    private char symbol;
+	
+	
+	public Solver(ArrayList<Integer> operands, ArrayList<Character> operators){
+        operandStack = new Stack<Integer>();
+        operatorStack = new Stack<Character>();
+        this.operands = operands;
+        this.operators = operators;
+    }
+	
+	public Solver() {
+		operandStack = new Stack<Integer>();
+        operatorStack = new Stack<Character>();
+	}
+	
+	public boolean calculate(){
+        try{
+            while(operands.size() != 0 || operators.size() != 0){
+                operandStack.push(operands.remove(0));
+                if(operatorStack.isEmpty()){                          //stack boşsa push
+                    operatorStack.push(operators.remove(0));
+                }
+                else if(operators.size() != 0){
+                    //en üsttekinin önceliği gelenden küçükse push
+                    if((operatorStack.peek() == '+' || operatorStack.peek()  == '-') && (operators.get(0) == '*' || operators.get(0) == '/')){
+                        operatorStack.push(operators.remove(0));
+                    }
+                    else{
+                        number2 = operandStack.pop();
+                        symbol = operatorStack.pop();
+                        number1 = operandStack.pop();
+
+                        if(symbol == '+'){
+                            operandStack.push(number1 + number2);
+                        }
+                        else if(symbol == '-'){
+                        	if(number1 - number2 < 0)
+                        		return false;                      		
+                            operandStack.push(number1 - number2);
+                        }
+                        else if(symbol == '*'){
+                            operandStack.push(number1 * number2);
+                        }
+                        else if(symbol == '/'){
+                        	if(number1 % number2 != 0) 
+                        		return false;	
+                            operandStack.push(number1 / number2);
+                        }
+                        operatorStack.push(operators.remove(0));
+                    }
+                }
+                else{
+                    number2 = operandStack.pop();
+                    symbol = operatorStack.pop();
+                    number1 = operandStack.pop();
+
+                    if(symbol == '+'){
+                        operandStack.push(number1 + number2);
+                    }
+                    else if(symbol == '-'){
+                    	if(number1 - number2 < 0)
+                    		return false;
+                        operandStack.push(number1 - number2);
+                    }
+                    else if(symbol == '*'){
+                        operandStack.push(number1 * number2);
+                    }
+                    else if(symbol == '/'){
+                    	if(number1 % number2 != 0)
+                    		return false;
+                        operandStack.push(number1 / number2);
+                    }
+                }
+            }
+
+            while(!operatorStack.isEmpty()){
+                number2 = operandStack.pop();
+                symbol = operatorStack.pop();
+                number1 = operandStack.pop();
+
+                if(symbol == '+'){
+                    operandStack.push(number1 + number2);
+                }
+                else if(symbol == '-'){
+                	if(number1 - number2 < 0) 
+                		return false;
+                    operandStack.push(number1 - number2);
+                }
+                else if(symbol == '*'){
+                    operandStack.push(number1 * number2);
+                }
+                else if(symbol == '/'){
+                	if(number1 % number2 != 0)
+                		return false;
+                    operandStack.push(number1 / number2);
+                }
+            }
+            result = operandStack.pop();
+        }
+        catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+	
+	public void setList(ArrayList<Integer> operands, ArrayList<Character> operators){
+        this.operands = operands;
+        this.operators = operators;
+    }
+	
+	public int getResult(){
+        return result;
+    }
+}
